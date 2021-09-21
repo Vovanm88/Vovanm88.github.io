ce117e017cd4549699873f1a1797b4056667c00e
Vladimir Melnikov <81383125+Vovanm88@users.noreply.github.com>
2021-09-21 23:06:41 +0300

    Update ContentCreater.js

--- ContentCreater.js
+++ ContentCreater.js
@@ -1,5 +1,7 @@
-let globalParent=document.getElementById("main");
-let toDel=document.getElementById("jsdontwork");
-toDel.remove();
+document.addEventListener('DOMContentLoaded', function(event){
+    let globalParent=document.getElementById("main");
+    let toDel=document.getElementById("jsdontwork");
+    toDel.remove();
+    globalParent.innerText="IT WORKS AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+});
 
-globalParent.innerText="IT WORKS AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
