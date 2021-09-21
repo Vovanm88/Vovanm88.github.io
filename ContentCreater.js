8574c88da5bfa78b30bf2ad63fa30bc16ee00736
Vladimir Melnikov <81383125+Vovanm88@users.noreply.github.com>
2021-09-21 22:54:57 +0300

    Add files via upload

---
+++ ContentCreater.js
@@ -0,0 +1,5 @@
+let globalParent=document.getElementById("main");
+let toDel=document.getElementById("jsdontwork");
+toDel.parentElement.removeChild(toDel);
+
+globalParent.innerText="IT WORKS AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
